0884ba73f0d9ec0ea09e7f54d0c693e62ebb07d3
Hayden Dunlap <your@email.here>
2023-04-25 15:26:50 -0500

    Create bootstrap table on packlist page

--- ghi/src/PackingList.jsx
+++ ghi/src/PackingList.jsx
@@ -42,38 +42,58 @@ const ListDetail = () => {
     };
 
 
+
     useEffect(() => {
         getData()
     }, [])
 
     return (
         <div className="container">
-            <div>
-                {/* {allInfo["daily"]?.map()} */}
-            </div>
-            <h1>{packingList?.name}</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Packed</th>
-                        <th>Item Name</th>
-                        <th>Quantity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {allPackingListItems?.map((item) => {
+            <div className="row p-3" style={{height: "7em"}}>
+                <div className="col-4">
+                    <h1>{packingList?.name}</h1>
+                </div>
+                <div className="col-6 card" style={{backgroundColor: "#AED9E0"}}>
 
-                        return (
-                            <tr key={item.id}>
-                                <td><input type="checkbox" checked={item.is_packed} value={item.is_packed} onChange={() => dispatch(handleIsPackedChange())} onClick={(e) => handleChange(e, item)}></input></td>
-                                <td>{item.name}</td>
-                                <td>{item.quantity}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <button><Link to={`/packinglist/${packingListID}/datelists`} >Edit</Link></button>
+                </div>
+            </div>
+            <div style={{width: "54em", fontSize: "1.25em", padding: "1em"}}>
+                <table className="card table" >
+                    <thead>
+                        <tr>
+                            <th style={{width:"5em", textAlign: "center"}}>Packed</th>
+                            <th style={{width:"44em", paddingLeft: "1em"}}>Item Name</th>
+                            <th style={{width:"5em", textAlign: "center"}}>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allPackingListItems?.map((item) => {
+
+                            return (
+                                <tr key={item.id}>
+                                    <td className="position-relative" style={{width:"5em"}}>
+                                        <input
+                                            className="position-absolute top-50 start-50 translate-middle form-check-input"
+                                            type="checkbox"
+                                            checked={item.is_packed}
+                                            value={item.is_packed}
+                                            onChange={() => dispatch(handleIsPackedChange())}
+                                            onClick={(e) => handleChange(e, item)}>
+                                        </input>
+                                    </td>
+                                    <td th style={{width:"44em", paddingLeft: "1em"}}>{item.name}</td>
+                                    <td style={{width: "5em", textAlign: "center"}}>{item.quantity}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div style={{padding: "1em"}}>
+                <button className="btn btn-secondary" >
+                    <Link to={`/packinglist/${packingListID}/datelists`} style={{color: "white", textDecoration: "none"}}>Edit List</Link>
+                </button>
+            </div>
         </div>
     )
 }
