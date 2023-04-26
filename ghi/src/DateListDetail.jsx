2a1c36ecb2f521c1f76d95b59389e0bdfd1ee0a9
KyleAMcGinley <kyleamcginley@gmail.com>
2023-04-26 13:23:43 -0400

    completed issue-number-8 added styling to date detail page

--- ghi/src/DateListDetail.jsx
+++ ghi/src/DateListDetail.jsx
@@ -2,7 +2,7 @@ import { React, useState }from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from 'react-router-dom';
-import { useGetDatesQuery, useGetWeatherInfoQuery, useGetItemsByPacklistQuery, useCreateItemMutation, useDeleteItemMutation} from "./services/Travelthreads";
+import { useGetDatesQuery, useGetWeatherInfoQuery, useGetItemsByPacklistQuery, useCreateItemMutation, useDeleteItemMutation, useGetOneListQuery} from "./services/Travelthreads";
 import { handleNameChange } from "./features/auth/createItem";
 
 
@@ -14,8 +14,12 @@ const DateDetail = () => {
     const params = useParams();
     var packingListID = params["id"]
 
+
+    const {data: packingListDetail} = useGetOneListQuery(params?.id)
+
+
     const {data: allDateLists} = useGetDatesQuery(params?.id, { skip: !params?.id })
-    console.log(allDateLists)
+
 
     const {data: packListItems} = useGetItemsByPacklistQuery(params.id)
 
@@ -56,62 +60,98 @@ const DateDetail = () => {
     }
 
 
+    const cardStyle = {
+        width: "300px",
+        height: "auto",
+        margin: "0 auto",
+        padding: "1rem",
+        marginBottom: "25px",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        transition: "all 0.3s linear",
+        backgroundColor: "#FFA69E",
+    };
+
+
+    const cardBodyStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "100%",
+    };
+
+
     return (
-        <div className="container" style={{display: "flex", justifyContent: "center"}}>
-            {allDateLists?.map((dateList) => {
-                let weatherCard = <div>Weather information not available</div>
-                let items = <div key=""></div>
-                if (allInfo !== undefined && allInfo.daily.time.indexOf(dateList.date) !== -1) {
-                    const index = allInfo.daily.time.indexOf(dateList.date)
-                    weatherCard = <div key={dateList.date} style={{display: "flex", alignItems: "center"}}>
-                    High: {allInfo.daily.temperature_2m_max[index]}
-                    Low: {allInfo.daily.temperature_2m_min[index]}
-                    Precipitation: {allInfo.daily.precipitation_probability_max[index]}</div>
-                }
+        <div className="container" style={{minWidth: "100%", height: "100vh", position: "fixed", overflow: "scroll", backgroundColor: "#AED9E0"}}>
+            <Link to={`/packinglist/${packingListID}`} className="header" style={{display: "flex", justifyContent: "center", margin: "50px 0px 50px 0px", textDecoration: "none", color: "black"}}>
+                <h1 style={{fontSize: "50px"}}>{packingListDetail?.name}</h1>
+            </Link>
+            <div className="info" style={{display:"grid", gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr"}}>
+                {allDateLists?.map((dateList) => {
+                    let weatherCard = <div style={{marginBottom: "25px"}}>Weather information not available</div>
+                    let items = <div key="" style={cardBodyStyle}></div>
+                    if (allInfo !== undefined && allInfo.daily.time.indexOf(dateList.date) !== -1) {
+                        const index = allInfo.daily.time.indexOf(dateList.date)
+                        weatherCard =
+                        <div key={dateList.date} style={{display:"flex", flexDirection: "column", alignItems: "start", width:"266px", marginBottom: "25px"}}>
+                            <div>High: {allInfo.daily.temperature_2m_max[index]}°F</div>
+                            <div>Low: {allInfo.daily.temperature_2m_min[index]}°F</div>
+                            <div>Precipitation: {allInfo.daily.precipitation_probability_max[index]}%</div>
+                        </div>
+                    }
 
-                if (packListItems !== undefined) {
-                    const filteredItems = packListItems.filter(item => {
-                        return item.date_list_id === dateList.id
-                    })
-                    items = filteredItems.map(item => {
-                        return (
-                            <div key={item.id}>
-                                <div>{item.name}: {item.quantity}</div>
-                                <Link to={`/packinglist/${packingListID}/items/${item.id}`} state={item}><button>Edit</button></Link>
-                                <button value={item.id} onClick={handleDelete}>Delete</button>
+                    if (packListItems !== undefined) {
+                        const filteredItems = packListItems.filter(item => {
+                            return item.date_list_id === dateList.id
+                        })
+                        items = filteredItems.map(item => {
+                            return (
+                                <div key={item.id} style={{display:"flex", justifyContent: "space-between", width: "266px"}}>
+                                    <div style={{margin: "0px 0px 15px 0px"}}>{item.name}: {item.quantity}</div>
+                                    <div className="buttons">
+                                        <Link to={`/packinglist/${packingListID}/items/${item.id}`} state={item}><button style={{height: "26px", fontSize: "12px", backgroundColor: "#6c757d", color: "white"}}>Edit</button></Link>
+                                        <button value={item.id} onClick={handleDelete} style={{height: "26px", fontSize: "12px", backgroundColor: "#6c757d", color: "white"}}>Delete</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+
+                    return (
+                        <div key={dateList.id} id={dateList.id} className="card" style={cardStyle}>
+                            <div style={cardBodyStyle}>
+                                <h1 style={{marginBottom:"15px"}}>{changeDateFormat(dateList.date)}</h1>
+                                {(dateList.description !== "") ?
+                                    (<div style={{textAlign: "center", marginBottom: "15px"}}><Link to={`/packinglist/${packingListID}/datelists/${dateList.id}`} state={dateList} style={{textDecoration: "none", color: "black"}}>{dateList.description}</Link></div>)
+                                :
+                                    (<div style={{textAlign: "center", marginBottom: "15px"}}><Link to={`/packinglist/${packingListID}/datelists/${dateList.id}`} state={dateList} style={{textDecoration: "none", color: "black"}}>Add Description</Link></div>)
+                                }
+                                <div>{weatherCard}</div>
+                                <div>{items}</div>
+                                <div className="input-div" style={{display: "flex"}}>
+                                    <div><input onChange={(e) => dispatch(handleNameChange(e.target.value))} style={{height: "26px"}}></input></div>
+                                    <div><button value={dateList.id} onClick={handleSubmit} style={{height: "26px", fontSize: "12px", backgroundColor: "#6c757d", color: "white"}}>Add Item</button></div>
+                                </div>
                             </div>
-                        )
-                    })
-                }
-
-                return (
-                    <div key={dateList.id} id={dateList.id} className="card" style={{width: "25rem", height: "25rem", padding:"2rem", margin:"1rem", alignItems:"center"}}>
-                        <h1>{changeDateFormat(dateList.date)}</h1>
-                        {(dateList.description !== "") ?
-                            (<div><Link to={`/packinglist/${packingListID}/datelists/${dateList.id}`} state={dateList}>{dateList.description}</Link></div>)
-                        :
-                            (<div><Link to={`/packinglist/${packingListID}/datelists/${dateList.id}`} state={dateList}>Add Description</Link></div>)
-                        }
-                        <div>{weatherCard}</div>
-                        <div>{items}</div>
-                        <div><input onChange={(e) => dispatch(handleNameChange(e.target.value))}></input></div>
-                        <div><button value={dateList.id} onClick={handleSubmit}>Add Item</button></div>
+                        </div>
+                    )
+                })}
+                <div className="card" style={{...cardStyle, marginBottom:"150px"}}>
+                    <h1>Other Items</h1>
+                        {misc_items?.map(item => {
+                            return (
+                                <div key={item.id} style={cardBodyStyle}>
+                                    <div key={item.id}>{item.name}: {item.quantity}</div>
+                                    <Link to={`/packinglist/${packingListID}/items/${item.id}`} state={item}><button style={{height: "26px", fontSize: "12px", backgroundColor: "#6c757d", color: "white"}}>Edit</button></Link>
+                                    <button value={item.id} onClick={handleDelete} style={{height: "26px", fontSize: "12px", backgroundColor: "#6c757d", color: "white"}}>Delete</button>
+                                </div>
+                            )
+                        })}
+                    <div className="input-div" style={{display: "flex"}}>
+                        <div><input onChange={(e) => dispatch(handleNameChange(e.target.value))} style={{height: "26px"}}></input></div>
+                        <div><button value={null} onClick={handleSubmit} style={{height: "26px", fontSize: "12px", backgroundColor: "#6c757d", color: "white"}}>Add Item</button></div>
                     </div>
-                )
-            })}
-            <div className="card" style={{width: "25rem", height: "25rem", padding:"2rem", margin:"1rem", alignItems:"center"}}>
-                <h1>Other Items</h1>
-                    {misc_items?.map(item => {
-                        return (
-                            <div key={item.id}>
-                                <div key={item.id}>{item.name}: {item.quantity}</div>
-                                <Link to={`/packinglist/${packingListID}/items/${item.id}`} state={item}><button>Edit</button></Link>
-                                <button value={item.id} onClick={handleDelete}>Delete</button>
-                            </div>
-                        )
-                    })}
-                <div><input onChange={(e) => dispatch(handleNameChange(e.target.value))}></input></div>
-                <div><button value={null} onClick={handleSubmit}>Add Item</button></div>
+                </div>
             </div>
         </div>
     )
