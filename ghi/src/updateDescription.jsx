f3ad343bf6a2a32ff3cb8df1588e41834142442e
KyleAMcGinley <kyleamcginley@gmail.com>
2023-04-26 08:03:42 -0400

    styled update pages

--- ghi/src/updateDescription.jsx
+++ ghi/src/updateDescription.jsx
@@ -36,21 +36,24 @@ const UpdateDescription = () => {
 
 
     return (
-        <div className="container">
-            <h1>Update Description</h1>
-            <h2>{date}</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="UpdateDescription__name">Description</label>
-                    <input
-                        type={"text"}
-                        id="UpdateDescription__name"
-                        value={description}
-                        onChange={handleDescriptionChange}
-                    />
-                </div>
-                <button type="submit">Update Description</button>
-            </form>
+        <div className="container" style={{display: "flex", justifyContent: "center", backgroundColor: "#AED9E0", minWidth: "100%", height: "100vh"}}>
+            <div className="card" style={{width: "50rem", height: "20rem", display: "flex", justifyContent: "center", alignItems: "center", marginTop: "50px"}}>
+                <h1>Update Description</h1>
+                <h2>{date}</h2>
+                <form onSubmit={handleSubmit} style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
+                    <div style={{margin: "15px 0px 15px"}}>
+                        <label htmlFor="UpdateDescription__name" style={{fontWeight: "bold", margin: "7px"}}>Description:</label>
+                        <textarea
+                            type={"textarea"}
+                            id="UpdateDescription__name"
+                            value={description}
+                            onChange={handleDescriptionChange}
+                            style={{height: "7.5rem", width: "15rem"}}
+                        />
+                    </div>
+                    <button type="submit" style={{backgroundColor: "#FFA69E", width: "100px"}}>Update</button>
+                </form>
+            </div>
         </div>
     )
 }
