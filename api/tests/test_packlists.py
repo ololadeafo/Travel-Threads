5f65542ab0d79f8b24583259030d0cafd0998940
Ross <rappel610@gmail.com>
2023-04-27 12:55:05 -0400

    Added unit test for get on packlists

--- api/tests/test_packlists.py
+++ api/tests/test_packlists.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from queries.packlists import PackListQueries
 from models import PackListIn, PacklistOut
 from authenticator import authenticator
+from typing import Optional
 # from datetime import date
 
 
@@ -25,6 +26,9 @@ class PacklistQueriesMock:
         return [PacklistOut(name="testing",start_date= "2023-04-27",end_date= "2023-04-29",country= "United States", state= "Florida", city= "Miami", id=5, user_id=user_id)]
     def delete(self, user_id: int, id: int) -> bool:
         return True
+    def get_one(self, id: int, user_id: int) -> Optional[PacklistOut]:
+        return PacklistOut(name="testing",start_date= "2023-04-27",end_date= "2023-04-29",country= "United States", state= "Florida", city= "Miami", id=5, user_id=user_id)
+
 
 
 def test_create_packlist():
@@ -66,3 +70,13 @@ def test_delete_packlist():
     res = client.delete('/api/packlist/5')
     assert res.json()
     app.dependency_overrides = {}
+
+def test_get_one_packlist():
+    app.dependency_overrides[PackListQueries] = PacklistQueriesMock
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+    res = client.get('/api/packlist/5')
+    data = res.json()
+    assert data["user_id"] == 10
+    assert data["id"] == 5
+    app.dependency_overrides = {}
+
