a3e92d950be1693912b688d53a124b0d85b80d18
KyleAMcGinley <kyleamcginley@gmail.com>
2023-04-28 15:11:26 -0400

    removed use dispatch import from date detail page

--- ghi/src/DateListDetail.jsx
+++ ghi/src/DateListDetail.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import {
   useGetDatesQuery,
